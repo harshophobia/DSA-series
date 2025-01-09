b8099116d03f5f97af82868eebbd3356a75d3df4
harshophobia <harshbarnawa.info@gmail.com>
2025-01-10 01:29:46 +0530

    Difficult Ques

--- Q16 Inverted RA Char Tri Pattern.cpp
+++ Q16 Inverted RA Char Tri Pattern.cpp
@@ -6,15 +6,21 @@ int main() {
 
     for(int i = 0;i < n;i++) {
 
-        for(int j = i;j < i;j++) {
+        for(int j = 0;j < i;j++) {
             cout << " ";
+        }
 
-            for(int j = 0;j < n - i;j++) {
-                cout << (i + 1)<< " ";
-            }
-
+        for(int j = 0;j < n - i;j++) {
+            cout << char('A' + i);
         }
         cout << endl;
     }
     return 0;
 }
+
+/* Output--
+AAAA
+ BBB
+  CC
+   D
+*/
